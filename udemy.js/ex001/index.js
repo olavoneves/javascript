1d7ca21d3b8303205ec766b7d51b8a5da38e91b5
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-02-08 15:07:55 -0300

    treino

---
+++ udemy.js/ex001/index.js
@@ -0,0 +1,18 @@
+console.log('JavaScript Teste001')
+
+let firstName = 'Olavo'
+let lastName = 'Porto'
+let fullName = 'Olavo Porto Neves'
+console.log(firstName)
+console.log(lastName)
+console.log(fullName)
+
+let colorCar = 'Blue'
+console.log(colorCar)
+
+let favoritSport = 'Soccer'
+console.log(favoritSport)
+
+const price = 27
+// price = 30
+console.log(price)
