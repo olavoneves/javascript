1a3923ce5fa2e57f633c9014151004e6a4e095df
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-02-11 11:39:20 -0300

    If and Else

---
+++ udemy.js/ex004/index.js
@@ -0,0 +1,16 @@
+let driverSpeed = 147
+
+if (driverSpeed >= 120) {
+    console.log('Driving too Very Fast')
+}
+
+else if (driverSpeed >= 80 && driverSpeed < 120) {
+    console.log('Ok')
+}
+
+else if (driverSpeed >= 40 && driverSpeed < 80) {
+    console.log('Normal')
+}
+
+else 
+    console.log('Driving too Slow')
