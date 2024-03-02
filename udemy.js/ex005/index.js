7ed03fb8ba1c3d01629fc668ca30086517daa014
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-03-02 12:08:41 -0300

    loop

---
+++ udemy.js/ex005/index.js
@@ -0,0 +1,15 @@
+let airportName = 'MSC'
+
+switch (airportName) {
+    case 'MSC':
+        console.log('MSC Cruzeiro')
+        break
+    case 'JFK':
+        console.log('John F. Kennedy')
+        break
+    case 'SEA':
+        console.log('Seattle')
+        break
+    default:
+        console.log('Unknown')
+}
---
+++ udemy.js/ex006/index.js
@@ -0,0 +1,34 @@
+for (number = 01; number <= 10; number++) {
+    console.log('Número ', number)
+}
+
+for (i = 15; i <= 30; i++) {
+    console.log('number ', i)
+}
+
+
+numberr = 40
+
+while (numberr <= 50) {
+    console.log('number ', numberr)
+    numberr++
+}
+
+
+ii = 50
+
+do {
+    console.log('numero ', ii)
+    ii++
+} while (ii <= 60)
+
+
+const myCar = {
+    model: 'Honda',
+    year: 1995,
+    km: 100000
+}
+
+for (let i in myCar) 
+    console.log(i, myCar[i])
+
