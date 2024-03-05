4c271a1836af9efb60a248aeb49d80dcc46fa620
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-03-05 17:24:48 -0300

    variaveis

---
+++ udemy.js/exercícios.ud/ex008/index.js
@@ -0,0 +1,18 @@
+
+console.log(Math.min(1.5, 7, 14, 3, 5))
+
+let message = 'My name is Olavo Neves'
+
+console.log(message.startsWith('My'))
+console.log(message.includes('Olavo'))
+console.log(message.split(9))
+
+
+const email01 = 'Hello Guys, The meeting is confirmed !! Ass: Olavo'
+const email02 = `Hello Guys, 
+The meeting is confirmed !! 
+Ass: Olavo`
+ 
+
+console.log(email01)
+console.log(email02)
