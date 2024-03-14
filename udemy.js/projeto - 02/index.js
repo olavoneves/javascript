1784022322645ee969c8a0fc0236675ac3708344
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-03-14 16:13:34 -0300

    calc tip

---
+++ udemy.js/projeto - 02/index.js
@@ -0,0 +1,7 @@
+
+function update() {
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+    console.log(bill, tipPercent, split)
+}
