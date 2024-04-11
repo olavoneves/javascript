f5ad5ddcfbd0004db3983b3c5c20dad260598b46
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-04-11 16:47:24 -0300

    calc Finish

--- udemy.js/projeto - 02/index.js
+++ udemy.js/projeto - 02/index.js
@@ -3,5 +3,15 @@ function update() {
     let bill = Number(document.getElementById('yourBill').value)
     let tipPercent = document.getElementById('tipInput').value
     let split = document.getElementById('splitInput').value
-    console.log(bill, tipPercent, split)
+
+    let tipValue = bill * tipPercent / 100
+    let bilTotal = bill + tipValue
+    let billEach = billTotal / split
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + ' %'
+    document.getElementById('tipValue').innerHTML = '$ ' + tipValue 
+    document.getElementById('totalWithTip').innerHTML = '$ ' + billTotal 
+    document.getElementById('splitValue').innerHTML = split
+    document.getElementById('billEach').innerHTML = bilTotal / split
+    
 }
