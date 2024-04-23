e069059d5e83f2aa1f05a153415ab63d390a7a4f
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-04-23 20:26:26 -0300

    projeto - game

--- udemy.js/projeto - game/index.js
+++ udemy.js/projeto - game/index.js
@@ -1,12 +1,46 @@
-let computerNumbers
+let computerNumber
 let userNumbers = []
+let attempts = 0
+let maxGuesses = 8
+
+function newGame() {
+    window.location.reload()
+}
 
 function inicio() {
-   computerNumbers = Math.floor(Math.random() * 100 + 1)
+   computerNumber = Math.floor(Math.random() * 100 + 1)
+   console.log(computerNumber)
 }
 
 function compareNumber() {
     const userNumber = Number(document.getElementById('inputBox').value)
     userNumbers.push(' ' + userNumber)
     document.getElementById('guesses').innerHTML = userNumbers
+
+    if (attempts < maxGuesses) {
+        if (userNumber > computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too high'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        }
+        else if (userNumber < computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too low'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        }
+        else {
+            document.getElementById('textOutput').innerHTML = 'Congratulations !!!'
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+            document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
+        }  
+    }
+
+    else {
+        document.getElementById('textOutput').innerHTML = 'You Lose !! The computer number was ' + computerNumber
+        document.getElementById('inputBox').setAttribute('Readonly', 'Readonly')
+    }
+    
 }
