54ab77fd3d456167dccfe0c09ef86a3e5952b6a9
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-05-23 17:10:24 -0300

    pictures

--- desafios/desafio - 02/index.js
+++ desafios/desafio - 02/index.js
@@ -1,1 +1,11 @@
-window.alert('Olá')
+function verificar() {
+        var data = new Date()
+        var ano = data.getFullYear()
+        var fAno = document.getElementById('ano')
+        var res = document.getElementById('res')
+        if (fAno.value.length == 0 || Number(fAno.value) > ano) {
+            window.alert('ERRO !')
+        } else {
+            
+        }
+}
