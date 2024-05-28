b6c8a758753b261e03a441ac375b7142a0a24556
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2024-05-28 16:43:02 -0300

    repetição

--- exercicios/ex012/index.js
+++ exercicios/ex012/index.js
@@ -10,3 +10,19 @@ do {
     console.log(`Idade : ${idade}`)
     idade++
 } while (idade > 15 && idade <= 19)
+
+// var contagem = 15
+// while (contagem <= 18) {
+   // console.log(contagem)
+    // c++
+// }
+
+for(var contagem = 1;contagem <= 18;contagem++) {
+    console.log(contagem)
+}
+
+console.log(' Vai começar...')
+for(var conta = 1;conta <= 18;conta++) {
+    console.log(conta)
+}
+console.log(' Fim ! ')
